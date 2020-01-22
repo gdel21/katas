df6528c221b2174a20907adf88af531caddf4314
gdel21 <56038834+gdel21@users.noreply.github.com>
2020-01-21 21:59:37 -0500

    loop

--- katas/8q.js
+++ katas/8q.js
@@ -21,3 +21,20 @@
 const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
     return mpg * fuelLeft >= distanceToPump ? true : false
   };
+
+
+//Training JS #10: loop statement --for
+//https://www.codewars.com/kata/5721a78c283129e416000999
+
+function pickIt(arr){
+    var odd=[],even=[];
+    for(let i = 0; i < arr.length; i++){
+      if(arr[i] % 2 == 0){
+        even.push(arr[i])
+      }
+      else{
+        odd.push(arr[i])
+      }
+    }
+    return [odd,even];
+  }
